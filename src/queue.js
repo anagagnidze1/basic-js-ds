66f481cf8b6fcc7e11f59eda143d1069f661fffa
Ana Gagnidze <anagagnidze32@gmail.com>
2024-03-03 18:31:53 +0400

    add

--- src/queue.js
+++ src/queue.js
@@ -14,20 +14,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.head;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    if (this.length === 0) {
+      this.head = {'value': value, 'next': null};
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+
+      current.next = {'value': value, 'next': null};
+    }
+    this.length++;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let current = this.head;
+    this.head = current.next;
+    this.length--
+    return current.value;
   }
 }
 
